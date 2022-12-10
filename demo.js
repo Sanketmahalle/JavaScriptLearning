1ef2364954a93838948ffa2d0c4b10242970ba50
sanket mahalle <sanketmahalle17@gmail.com>
2022-12-10 12:10:56 +0530

    assignment

---
+++ demo.js
@@ -0,0 +1,18 @@
+function noArgu(){
+    console.log("This Function  with no argument");
+}
+noArgu();
+function personalDetails(firstName,lastName,collegeName){
+    console.log("This Function  with no argument" + firstName ,+lastName,+collegeName);
+}
+personalDetails("Sanket","Mahalle","PRMCEAM");
+// function swapValue(v1,v2){
+// console.log("Before Swap"+ v1,v2);
+// var temp=v1;
+// v1=v2;
+// v2=temp;
+// console.log("After Swap"+v1,v2);
+// }
+// swapValue(300 ,400);
+// swapValue(2,4);
+// swapValue(2,4);
